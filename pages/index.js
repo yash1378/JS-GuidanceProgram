3fffb62239828e9a509b0277c35a2bf1eaf2686c
yash <slayycasm997@gmail.com>
2023-10-08 18:51:59 +0530

    new frontend2

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,7 @@ const HomePage = () => {
     router.push("/login");
   };
 
-  const goToRegister = () => {
-    router.push("/registration");
-  };
+
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-white-500 via-blue-600 to-blue-700 text-white">
@@ -24,11 +22,6 @@ const HomePage = () => {
           <button onClick={goToLogIn} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
               Log In
-            </span>
-          </button>
-          <button onClick={goToRegister} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Register for Program
             </span>
           </button>
         </div>
