6f7e307651bffa889377d16cddb9e8041380b7f4
yash <slayycasm997@gmail.com>
2023-11-09 14:55:34 +0530

    availability issue sorted in frontend also

--- pages/mentavail.js
+++ pages/mentavail.js
@@ -206,6 +206,7 @@ function Registration({ data }) {
               <span className="relative px-20 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Submit
               </span>
+
             </button>
 
             {/* Subscription Type Field */}
