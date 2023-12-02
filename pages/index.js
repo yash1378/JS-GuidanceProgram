d8919aa33bea7359c9f5c5c7b3bc65c2882d705f
yash1378 <slayycasm997@gmail.com>
2023-12-02 11:32:43 +0530

    index updated

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const HomePage = () => {
         <title>Home</title>
       </Head>
       <div className="text-center">
-        <h1 className="text-[100px]  text-black font-extrabold mb-6">Welcome to Your App</h1>
+        <h1 className="text-[50px]  text-black font-extrabold mb-6">Welcome to Your App</h1>
         <div className="space-y-4 text-[60px]">
           <button onClick={goToLogIn} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-Damion-cursive text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
